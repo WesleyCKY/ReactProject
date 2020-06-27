65d4e9f9faf558877fc0742094d26f3286ce0e4e
Wesley chin <wesleychin@192.168.124.7>
2020-06-27 18:42:38 +0800

    Updates

---
+++ precache-manifest.0aa571accb087b7d896ab8b29e1bbd43.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e280398fae5aa96a7ad5328e48e4db",
+    "url": "/.github.io/MaskSaver-web/index.html"
+  },
+  {
+    "revision": "d12e7585a84a8f51ee4b",
+    "url": "/.github.io/MaskSaver-web/static/css/main.b18c8254.chunk.css"
+  },
+  {
+    "revision": "4dcca64faf493ca94523",
+    "url": "/.github.io/MaskSaver-web/static/js/2.e37d9f12.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/.github.io/MaskSaver-web/static/js/2.e37d9f12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d12e7585a84a8f51ee4b",
+    "url": "/.github.io/MaskSaver-web/static/js/main.d1c2e6fa.chunk.js"
+  },
+  {
+    "revision": "bf46f2ea133655a36d2b",
+    "url": "/.github.io/MaskSaver-web/static/js/runtime-main.e5ff4664.js"
+  },
+  {
+    "revision": "8d8b68c3b059351e0c007e150f2902ee",
+    "url": "/.github.io/MaskSaver-web/static/media/image1.8d8b68c3.jpeg"
+  },
+  {
+    "revision": "46fae4fce88d90f1996616a2cac5939d",
+    "url": "/.github.io/MaskSaver-web/static/media/image2.46fae4fc.jpeg"
+  },
+  {
+    "revision": "9f9dd530ee5ccde5d138e5ea12676bca",
+    "url": "/.github.io/MaskSaver-web/static/media/image3.9f9dd530.jpg"
+  },
+  {
+    "revision": "55bfae6c432e433f91445b19758b955a",
+    "url": "/.github.io/MaskSaver-web/static/media/image4.55bfae6c.png"
+  },
+  {
+    "revision": "da54168d9c23736eb9356d5d54ff5f2c",
+    "url": "/.github.io/MaskSaver-web/static/media/image5.da54168d.png"
+  },
+  {
+    "revision": "720f884c656440988464ec1451a2d7cb",
+    "url": "/.github.io/MaskSaver-web/static/media/image6.720f884c.png"
+  },
+  {
+    "revision": "6f8caa150fa8c61ed104f7a17519d42f",
+    "url": "/.github.io/MaskSaver-web/static/media/image7.6f8caa15.png"
+  },
+  {
+    "revision": "c449765825ae23b627174d2b54ed0924",
+    "url": "/.github.io/MaskSaver-web/static/media/image8.c4497658.png"
+  }
+]);
